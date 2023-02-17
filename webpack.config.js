1ec51a8161b01403045762504d5e1f38af953dca
Alexander <47733425+lxndr-rl@users.noreply.github.com>
2023-02-17 10:48:11 -0500

    optimizado en producción

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 
 module.exports = async function _(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
+  if (config.mode === "development") {
+    config.devServer.compress = false;
+  }
+  if (config.mode === "production") {
+    config.optimization.minimize = false;
+  }
   // Customize the config before returning it.
   return config;
 };
